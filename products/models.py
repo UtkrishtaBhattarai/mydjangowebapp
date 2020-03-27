c56dcfea8b299711973540e2f67498cfced8de46
Utkrishta Bhattarai <bhattarai.utk1@gmail.com>
2020-03-27 23:41:33 +0545

    Website building

--- products/models.py
+++ products/models.py
@@ -5,4 +5,9 @@ class Product(models.Model):
     name=models.CharField(max_length=255)
     price=models.FloatField()
     stock=models.IntegerField()
-    image_url= models.CharField()
+    image_url= models.CharField(max_length=3012)
+
+class Offer(models.Model):
+    code=models.CharField(max_length=30)
+    description=models.CharField(max_length=255)
+    discount=models.FloatField()
--- products/views.py
+++ products/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    products=(Product.objects.all())
+    return render(request,'index.html',{'products':products})
 
 def newproducts(request):
     return HttpResponse("New Products")
+
+def oldproducts(request):
+    return HttpResponse("Old Products")
